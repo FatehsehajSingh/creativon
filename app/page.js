037fb0b9c0ea719ac3912e2b29ed47d4631e671a
Fatehsehaj Singh <fatehsehaj1234@gmail.com>
2025-10-03 17:45:14 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -11,10 +11,10 @@ export default function Home() {
         </p>
         <div>
           <Link href="/about">
-          <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Read More</button>
+            <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Read More</button>
           </Link>
           <Link href="/login">
-          <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Here</button>
+            <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Here</button>
           </Link>
         </div>
       </div>
@@ -41,15 +41,17 @@ export default function Home() {
         </div>
       </div>
 
-            <div className="bg-white h-1 opacity-10"></div>
+      <div className="bg-white h-1 opacity-10"></div>
 
 
-      <div className="text-white container mx-auto flex flex-col text-center justify-center pb-32 pt-14">
-                <h2 className=" text-3xl font-bold text-center mb-14">Learn More About Us</h2>
+      <div className="text-white container mx-auto flex flex-col text-center items-center justify-center pb-32 pt-14">
+        <h2 className=" text-3xl font-bold text-center mb-14">Learn More About Us</h2>
+        <div>
 
-              <iframe  src="https://www.youtube.com/embed/CF52N-w4anI?si=G_JINVwYOnOa-jTt" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe  src="https://www.youtube.com/embed/CF52N-w4anI?si=G_JINVwYOnOa-jTt" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        </div>
 
-      
+
       </div>
     </>
   );
